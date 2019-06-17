32779cee968c7ff14bb3e2be211e5cd02f340a9b
Joachim Dalen <me@d3v.no>
2019-06-17 09:47:08 +0200

    Add exception handling to background worker

--- src/TimeSave/MainForm.cs
+++ src/TimeSave/MainForm.cs
@@ -10,6 +10,7 @@ namespace TimeSave
     public partial class MainForm : Form
     {
         private bool _isRunning = false;
+        private int _failCount = 0;
         public MainForm()
         {
             InitializeComponent();
@@ -22,26 +23,53 @@ namespace TimeSave
 
         private void BwPortFetcher_DoWork(object sender, DoWorkEventArgs e)
         {
-            var ports = SerialPort.GetPortNames();
-            BwPortFetcher.ReportProgress(0, ports);
-            Debug.WriteLine("Did work, sleeping for 5 seconds");
-            Thread.Sleep(5000);
+            try
+            {
+                var ports = SerialPort.GetPortNames();
+                BwPortFetcher.ReportProgress(0, ports);
+                Debug.WriteLine("Did work, sleeping for 5 seconds");
+                Thread.Sleep(5000);
+
+            }
+            catch
+            {
+                _failCount++;
+            }
+
         }
 
         private void BwPortFetcher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            if (e.UserState is object[] ports && (ports.Length != 0 && !_isRunning))
+            try
             {
-                Debug.WriteLine("Found ports, appending to list");
-                LbPorts.Items.Clear();
-                LbPorts.Items.AddRange(ports);
+                if (e.UserState is object[] ports && (ports.Length != 0 && !_isRunning))
+                {
+                    _failCount = 0;
+                    Debug.WriteLine("Found ports, appending to list");
+                    LbPorts.Items.Clear();
+                    LbPorts.Items.AddRange(ports);
+                }
+                else
+                {
+                    Debug.WriteLine("No ports found, restarting worker");
+                    if (_failCount >= 3)
+                    {
+                        MessageBox.Show(@"An error occurred while checking for new ports. Please restart the application.",
+                            @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Debug.WriteLine("Stopped due to fail count");
+                    }
+                    else
+                    {
+                        // Restart the worker
+                        BwPortFetcher.RunWorkerAsync();
+                    }
+                }
             }
-            else
+            catch
             {
-                Debug.WriteLine("No ports found, restarting worker");
-                // Restart the worker
-                BwPortFetcher.RunWorkerAsync();
+                _failCount++;
             }
+
         }
 
         private void MainForm_Load(object sender, EventArgs e)
