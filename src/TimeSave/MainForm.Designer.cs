fcd1de8ae3b03858c070b22b0bc6a7c1014b3bfe
Joachim Dalen <me@d3v.no>
2019-06-17 09:22:40 +0200

    Add main UI elements

--- src/TimeSave/MainForm.Designer.cs
+++ src/TimeSave/MainForm.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LbPorts = new System.Windows.Forms.ListBox();
+            this.BtnStartRead = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.filToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LbPorts
+            // 
+            this.LbPorts.FormattingEnabled = true;
+            this.LbPorts.Location = new System.Drawing.Point(12, 44);
+            this.LbPorts.Name = "LbPorts";
+            this.LbPorts.Size = new System.Drawing.Size(211, 95);
+            this.LbPorts.TabIndex = 0;
+            // 
+            // BtnStartRead
+            // 
+            this.BtnStartRead.Location = new System.Drawing.Point(12, 146);
+            this.BtnStartRead.Name = "BtnStartRead";
+            this.BtnStartRead.Size = new System.Drawing.Size(211, 23);
+            this.BtnStartRead.TabIndex = 1;
+            this.BtnStartRead.Text = "Start";
+            this.BtnStartRead.UseVisualStyleBackColor = true;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(12, 175);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(211, 23);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filToolStripMenuItem,
+            this.sourceToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Size = new System.Drawing.Size(235, 24);
+            this.MainMenuStrip.TabIndex = 3;
+            this.MainMenuStrip.Text = "menuStrip1";
+            // 
+            // filToolStripMenuItem
+            // 
+            this.filToolStripMenuItem.Name = "filToolStripMenuItem";
+            this.filToolStripMenuItem.Size = new System.Drawing.Size(31, 20);
+            this.filToolStripMenuItem.Text = "Fil";
+            // 
+            // sourceToolStripMenuItem
+            // 
+            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
+            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.sourceToolStripMenuItem.Text = "Source";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(235, 205);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnStartRead);
+            this.Controls.Add(this.LbPorts);
+            this.Controls.Add(this.MainMenuStrip);
+            this.MainMenuStrip = this.MainMenuStrip;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeSave";
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LbPorts;
+        private System.Windows.Forms.Button BtnStartRead;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem filToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
     }
 }
 
