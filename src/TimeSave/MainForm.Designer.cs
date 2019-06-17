7e3d2041e581a4cf495dfce1a1d5df553728b802
Joachim Dalen <me@d3v.no>
2019-06-17 09:39:59 +0200

    Add background worker to constantly check for COM ports

--- src/TimeSave/MainForm.Designer.cs
+++ src/TimeSave/MainForm.Designer.cs
@@ -32,8 +32,9 @@
             this.BtnStartRead = new System.Windows.Forms.Button();
             this.BtnExit = new System.Windows.Forms.Button();
             this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
-            this.filToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BwPortFetcher = new System.ComponentModel.BackgroundWorker();
             this.MainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,12 +43,12 @@
             this.LbPorts.FormattingEnabled = true;
             this.LbPorts.Location = new System.Drawing.Point(12, 44);
             this.LbPorts.Name = "LbPorts";
-            this.LbPorts.Size = new System.Drawing.Size(211, 95);
+            this.LbPorts.Size = new System.Drawing.Size(211, 56);
             this.LbPorts.TabIndex = 0;
             // 
             // BtnStartRead
             // 
-            this.BtnStartRead.Location = new System.Drawing.Point(12, 146);
+            this.BtnStartRead.Location = new System.Drawing.Point(12, 106);
             this.BtnStartRead.Name = "BtnStartRead";
             this.BtnStartRead.Size = new System.Drawing.Size(211, 23);
             this.BtnStartRead.TabIndex = 1;
@@ -56,7 +57,7 @@
             // 
             // BtnExit
             // 
-            this.BtnExit.Location = new System.Drawing.Point(12, 175);
+            this.BtnExit.Location = new System.Drawing.Point(12, 135);
             this.BtnExit.Name = "BtnExit";
             this.BtnExit.Size = new System.Drawing.Size(211, 23);
             this.BtnExit.TabIndex = 2;
@@ -66,39 +67,46 @@
             // MainMenuStrip
             // 
             this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.filToolStripMenuItem,
-            this.sourceToolStripMenuItem});
+            this.FileToolStripMenuItem,
+            this.SourceToolStripMenuItem});
             this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainMenuStrip.Name = "MainMenuStrip";
             this.MainMenuStrip.Size = new System.Drawing.Size(235, 24);
             this.MainMenuStrip.TabIndex = 3;
             this.MainMenuStrip.Text = "menuStrip1";
             // 
-            // filToolStripMenuItem
+            // FileToolStripMenuItem
             // 
-            this.filToolStripMenuItem.Name = "filToolStripMenuItem";
-            this.filToolStripMenuItem.Size = new System.Drawing.Size(31, 20);
-            this.filToolStripMenuItem.Text = "Fil";
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
             // 
-            // sourceToolStripMenuItem
+            // SourceToolStripMenuItem
             // 
-            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
-            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
-            this.sourceToolStripMenuItem.Text = "Source";
+            this.SourceToolStripMenuItem.Name = "SourceToolStripMenuItem";
+            this.SourceToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.SourceToolStripMenuItem.Text = "Source";
+            this.SourceToolStripMenuItem.Click += new System.EventHandler(this.SourceToolStripMenuItem_Click);
+            // 
+            // BwPortFetcher
+            // 
+            this.BwPortFetcher.WorkerReportsProgress = true;
+            this.BwPortFetcher.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BwPortFetcher_DoWork);
+            this.BwPortFetcher.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BwPortFetcher_RunWorkerCompleted);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(235, 205);
+            this.ClientSize = new System.Drawing.Size(235, 170);
             this.Controls.Add(this.BtnExit);
             this.Controls.Add(this.BtnStartRead);
             this.Controls.Add(this.LbPorts);
             this.Controls.Add(this.MainMenuStrip);
-            this.MainMenuStrip = this.MainMenuStrip;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TimeSave";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.MainMenuStrip.ResumeLayout(false);
             this.MainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -112,8 +120,9 @@
         private System.Windows.Forms.Button BtnStartRead;
         private System.Windows.Forms.Button BtnExit;
         private System.Windows.Forms.MenuStrip MainMenuStrip;
-        private System.Windows.Forms.ToolStripMenuItem filToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SourceToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker BwPortFetcher;
     }
 }
 
--- src/TimeSave/MainForm.cs
+++ src/TimeSave/MainForm.cs
@@ -1,20 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TimeSave
 {
     public partial class MainForm : Form
     {
+        private bool _isRunning = false;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void SourceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/joachimdalen/TimeSave");
+        }
+
+        private void BwPortFetcher_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var ports = SerialPort.GetPortNames();
+            BwPortFetcher.ReportProgress(0, ports);
+            Debug.WriteLine("Did work, sleeping for 5 seconds");
+            Thread.Sleep(5000);
+        }
+
+        private void BwPortFetcher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.UserState is object[] ports && (ports.Length != 0 && !_isRunning))
+            {
+                Debug.WriteLine("Found ports, appending to list");
+                LbPorts.Items.Clear();
+                LbPorts.Items.AddRange(ports);
+            }
+            else
+            {
+                Debug.WriteLine("No ports found, restarting worker");
+                // Restart the worker
+                BwPortFetcher.RunWorkerAsync();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BwPortFetcher.RunWorkerAsync();
         }
     }
 }
